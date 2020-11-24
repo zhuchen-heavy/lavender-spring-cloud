2501f7efa8a1695a9d1ca7f6768d17715482d207
zhuchen8 <zhuchen8@hikvision.com.cn>
2020-11-24 19:28:54 +0800

    1：updata

--- lavender-nacos/lavender-nacos-discovery/lavender-nacos-discovery-consumer/src/main/java/com/lavender/nacos/consumer/controller/ConsumerController.java
+++ lavender-nacos/lavender-nacos-discovery/lavender-nacos-discovery-consumer/src/main/java/com/lavender/nacos/consumer/controller/ConsumerController.java
@@ -5,13 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-@Profile()
 @RestController
 public class ConsumerController {
 
